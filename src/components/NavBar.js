43b176f2ef9a0a3abd2e97e292b4573ae5efb30c
joe-toni <joefaburrieta@gmail.com>
2022-11-06 15:26:43 -0800

    started new react project included tailwind for styling

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function NavBar(){
+    return(
+        <>
+            <header className="bg-gray-800">
+                <a className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"> Home</a>
+                <a>About Me</a>
+                <a>Projects</a>
+            </header>
+        </>
+        )
+    }
